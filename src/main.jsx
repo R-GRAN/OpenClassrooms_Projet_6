8e9a6a9c5a5548e29d0896319091b03bf00cdca9
Remy <remy.grangenois@gmail.com>
2024-02-25 12:49:04 +0100

    Ajout des data.json et des images

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Propos from "./pages/Propos/index.jsx";
@@ -15,6 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
-    <App />
   </React.StrictMode>
 );
