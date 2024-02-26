d1538d34166cb02fdb3654970fb36525fba23bd3
Remy <remy.grangenois@gmail.com>
2024-02-26 23:40:01 +0100

    Page 404 desktop stylisée

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Error() {
-  return <div>error</div>;
+  return (
+    <div className="error">
+      <span>404</span>
+      <p>Oups! La page que vous demandez n existe pas</p>
+      <Link to="/">Retourner sur la page d’accueil</Link>
+    </div>
+  );
 }
 
 export default Error;
