0e300584f21eed2f85ff37516bf1b5606886b349
Remy <remy.grangenois@gmail.com>
2024-02-27 12:27:39 +0100

    Card créé - à modifier

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-import Banner from "@/components/others/Banner.jsx"
+import Banner from "@/components/others/Banner.jsx";
 
 function Home() {
-  return ( <div>
-    <Banner text="Chez vous, partout et ailleurs"/>
-    </div>);
+  return (
+    <div>
+      <Banner text="Chez vous, partout et ailleurs" />
+    </div>
+  );
 }
 
 export default Home;
