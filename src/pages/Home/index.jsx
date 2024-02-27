b8f2df224b81919597d20abbf5a901c1de7ce5e5
Remy <remy.grangenois@gmail.com>
2024-02-27 12:47:46 +0100

    patch - import gallery

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
-
-import Banner from "@/components/others/Banner.jsx"
+import Gallery from "@/components/others/Gallery.jsx";
+import Banner from "@/components/others/Banner.jsx";
 
 function Home() {
-  return ( <div>
-    <Banner text="Chez vous, partout et ailleurs"/>
-    <Gallery/>
-    </div>);
+  return (
+    <div>
+      <Banner text="Chez vous, partout et ailleurs" />
+      <Gallery />
+    </div>
+  );
 }
 export default Home;
