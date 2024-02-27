25f839b76d8749cb056b679e272c5f8e48dc9e61
Remy <remy.grangenois@gmail.com>
2024-02-27 11:40:45 +0100

    Gallery créée - à modifier

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import Banner from "@/components/others/Banner.jsx"
+import Gallery from "@/components/others/Gallery";
 
 function Home() {
   return ( <div>
     <Banner text="Chez vous, partout et ailleurs"/>
+    <Gallery/>
     </div>);
 }
 
