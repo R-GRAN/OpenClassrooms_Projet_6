a3b1b4124d36fa48fa78024739951008e4481065
Remy <remy.grangenois@gmail.com>
2024-02-28 03:22:34 +0100

    Card à revoir

--- src/components/others/Card.jsx
+++ src/components/others/Card.jsx
@@ -1,7 +1,14 @@
-function Card(){
-    return <article className="card">
-        <h2>hello</h2>
+function Card(props) {
+  function handleClick(evt) {
+    evt.preventDefault();
+    console.log("Hello");
+  }
+  return (
+    <article className="card" onClick={(evt) => handleClick(evt)}>
+      <img src={props.cover} alt="" />
+      <h2>{props.title}</h2>
     </article>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/others/Gallery.jsx
+++ src/components/others/Gallery.jsx
@@ -1,7 +1,13 @@
-function Gallery(){
-    return <section className="gallery">
+import datas from "@/assets/data/data.json";
+import Card from "./Card";
 
+function Gallery() {
+  return (
+    <section className="gallery">
+      {datas.map((data) => (
+        <Card key={`${data.id}-${data.title}`} title={data.title} />
+      ))}
     </section>
+  );
 }
-
-export default Gallery
+export default Gallery;
