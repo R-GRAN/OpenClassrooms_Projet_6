8c0a0a437c8f40a6e75c3b55c7edaa4007a3526e
Remy <remy.grangenois@gmail.com>
2024-03-06 01:42:01 +0100

    Lien vers la page ok

--- src/components/others/Card.jsx
+++ src/components/others/Card.jsx
@@ -1,17 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 function Card(props) {
   const { accommodation } = props;
 
-  console.log(accommodation);
-
   return (
-
+<Link to={`/logement/${accommodation.id}`}>
       <article className="card">
         <img src={accommodation.cover} />
         <h2>{accommodation.title}</h2>
       </article>
-
+</Link>
   );
 }
 
