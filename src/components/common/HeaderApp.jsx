652b2116fb1bd877cd781dd33dc4f91dbfbc454d
Remy <remy.grangenois@gmail.com>
2024-03-12 23:32:02 +0100

    Mise en place de NavLink au lieu de Link dans le header

--- src/components/common/HeaderApp.jsx
+++ src/components/common/HeaderApp.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "@/assets/images/LOGO.svg";
 
 function HeaderApp() {
@@ -6,8 +6,18 @@ function HeaderApp() {
     <header>
       <img src={logo} alt="logo de Kasa" />
       <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/propos">A Propos</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/propos"
+          className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+        >
+          A Propos
+        </NavLink>
       </nav>
     </header>
   );
