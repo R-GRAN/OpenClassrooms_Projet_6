3738e7147fdfbc0dc0ac5579e9b9786ff7cffe82
Remy <remy.grangenois@gmail.com>
2024-03-23 12:08:41 +0100

    correctif mineur : couleur background du collapse, alt du banner, correctif slideshow et cursor pointer sur button

--- src/components/others/Banner.jsx
+++ src/components/others/Banner.jsx
@@ -1,13 +1,10 @@
 function Banner({ picture, text }) {
   return (
     <div className="banner">
-      <img src={picture} alt="" />
-      <div className="banner_title">
-        <h1>{text}</h1>
-      </div>
+      <img src={picture} alt="une image d'arriere plan" />
+      <div className="banner_title">{text ? <h1>{text}</h1> : null}</div>
     </div>
   );
 }
-
 
 export default Banner;
